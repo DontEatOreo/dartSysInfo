9d26cf494b633c73488a331ee61230adf2e796ca
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2022-01-29 19:26:41 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,18 +1,17 @@
 import 'dart:io';
 
-void main() {
+Future<void> main() async {
   systemInformation();
 }
 
-void systemInformation() {
+Future<void> systemInformation() async {
   print("OS: ${Platform.operatingSystem}");
   print("OS Version: ${Platform.operatingSystemVersion}");
-  print("OS HostName: ${Platform.localHostname}");
-  print("CPU of Cores: ${Platform.numberOfProcessors / 2}");
+  print("OS Hostname: ${Platform.localHostname}");
+  print("CPU of Cores: ${Platform.numberOfProcessors.toInt() ~/ 2}");
   print("CPU of Threads: ${Platform.numberOfProcessors}");
   print("Locale: ${Platform.localeName}");
   print("Path Separator: ${Platform.pathSeparator}");
   print("Dart Binary Path: ${Platform.executable}");
   print("Dart Version: ${Platform.version}");
-  print("Environment Table: ${Platform.environment}");
 }
